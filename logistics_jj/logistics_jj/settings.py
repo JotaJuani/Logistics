3fe474d2ba5f1b8ec064725cf915c920c58fe93e
jotajuani <juanijimenez809@gmail.com>
2025-01-09 23:28:37 -0300

    reset pw added

--- logistics_jj/logistics_jj/settings.py
+++ logistics_jj/logistics_jj/settings.py
@@ -20,6 +20,8 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = config('EMAIL_HOST_USER')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
